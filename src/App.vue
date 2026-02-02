64c139ddecbf2f75c8890a6bfe19b52a6b2aa8e6
SvenNeli25 <petersevsek7@gmail.com>
2026-02-02 12:52:27 +0100

    obrazec za vnos vaj

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
-<script setup></script>
 
 <template>
   <v-app>
     <v-main>
       <v-container>
         <h1 class="mb-5"><v-icon icon="mdi-dumbbell"></v-icon> Dobrodošli v gym app</h1>
+        
+        <v-card class="mb-5 pa-4">
+          <h3> Dodaj Vajo! </h3>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field label="Ime" v-model="novaVaja.name"></v-text-field>
+            </v-col>
+            <v-col cols="6" md="4">
+              <v-text-field label="Ponovitve" v-model="novaVaja.reps" type="number"></v-text-field>
+            </v-col>
+            <v-col cols="6" md="4">
+              <v-text-field label="Kalorije" v-model="novaVaja.calories" type="number"></v-text-field>
+            </v-col>
+
+
+          </v-row>
+          <v-btn color="primary" @click="shraniVajo">Shrani Vajo</v-btn>
+
+        </v-card>
+
 
         <v-card>
           <v-list lines="two">
@@ -22,7 +41,7 @@
             </v-list-item>
 
             <v-list-item v-if="workouts.length === 0">
-              <v-list-item-title>...</v-list-item-title>
+              <v-list-item-title>...BIP BOP...</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-card>
@@ -36,6 +55,40 @@
 import { ref, onMounted } from "vue"
 
 const workouts = ref([])
+
+const novaVaja = ref({
+  name: '',
+  reps: null,
+  calories: null
+})
+
+async function shraniVajo() {
+  const podatki = {
+    name: novaVaja.value.name,
+    reps: novaVaja.value.reps,
+    calories: novaVaja.value.calories
+  }
+
+  try{
+    const response = await fetch('http://127.0.0.1:8000/api/workouts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(podatki)
+    })
+    
+    const shranjenaVaja = await response.json()
+    workouts.value.push(shranjenaVaja)
+
+    novaVaja.value.name = ''
+    novaVaja.value.reps = ''
+    novaVaja.value.calories = ''
+
+  } catch (error) {
+    console.error("napaka pri shranjevanju vaje: ", error)
+  }
+}
 
 onMounted(async () => {
   try{
