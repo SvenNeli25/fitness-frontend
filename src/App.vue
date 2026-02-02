08dbf2c4130b8e41e97173ab5ddd663b00e7f284
SvenNeli25 <petersevsek7@gmail.com>
2026-02-02 15:50:28 +0100

    CRUD (post, put, delete) koncan

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <h1 class="mb-5"><v-icon icon="mdi-dumbbell"></v-icon> Dobrodošli v gym app</h1>
         
         <v-card class="mb-5 pa-4">
-          <h3> Dodaj Vajo! </h3>
+          <h3> {{ urejamoId ? 'Uredi Vajo' : 'Dodaj Vajo' }}</h3>
           <v-row>
             <v-col cols="12" md="4">
               <v-text-field label="Ime" v-model="novaVaja.name"></v-text-field>
@@ -20,7 +20,7 @@
 
 
           </v-row>
-          <v-btn color="primary" @click="shraniVajo">Shrani Vajo</v-btn>
+          <v-btn color="primary" @click="shraniVajo">{{ urejamoId ? 'Posodobi' : 'Shrani' }}</v-btn>
 
         </v-card>
 
@@ -38,6 +38,28 @@
               <template v-slot:prepend>
                 <v-icon icon="mdi-run" color="blue"></v-icon>
               </template>
+              
+              <template v-slot:append>
+                <v-btn 
+                  icon="mdi-pencil" 
+                  color="orange"
+                  variant="text"
+                  class="mr-2"
+                  @click="napolniObrazec(workout)"
+                >
+                  
+                </v-btn>
+
+
+                <v-btn 
+                  icon="mdi-delete" 
+                  color="red"
+                  variant="text"
+                  @click="izbrisiVajo(workout.id)"
+                >
+                  
+                </v-btn>
+              </template>
             </v-list-item>
 
             <v-list-item v-if="workouts.length === 0">
@@ -51,6 +73,7 @@
 
 </template>
 
+
 <script setup>
 import { ref, onMounted } from "vue"
 
@@ -63,6 +86,12 @@ const novaVaja = ref({
 })
 
 async function shraniVajo() {
+
+  if(!novaVaja.value.name || !novaVaja.value.reps || !novaVaja.value.calories){
+    alert("Prosim izpolnite vsa polja")
+    return
+  }
+
   const podatki = {
     name: novaVaja.value.name,
     reps: novaVaja.value.reps,
@@ -70,24 +99,65 @@ async function shraniVajo() {
   }
 
   try{
-    const response = await fetch('http://127.0.0.1:8000/api/workouts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(podatki)
-    })
-    
-    const shranjenaVaja = await response.json()
-    workouts.value.push(shranjenaVaja)
+    let response;
 
-    novaVaja.value.name = ''
-    novaVaja.value.reps = ''
-    novaVaja.value.calories = ''
-
-  } catch (error) {
+    if(urejamoId.value){
+      response = await fetch(`http://127.0.0.1:8000/api/workouts/${urejamoId.value}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(podatki)
+      })
+      const posodobljenaVaja = await response.json()
+      const index = workouts.value.findIndex(vaja => vaja.id === urejamoId.value)
+      if (index !== -1) {
+        workouts.value[index] = posodobljenaVaja
+      }
+    } else {
+      response = await fetch('http://127.0.0.1:8000/api/workouts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(podatki)
+      })
+      const shranjenaVaja = await response.json()
+      workouts.value.push(shranjenaVaja)
+  }
+  preklicUrejanja()
+}catch (error) {
     console.error("napaka pri shranjevanju vaje: ", error)
   }
+}
+
+async function izbrisiVajo(id) {
+  try{
+    await fetch(`http://127.0.0.1:8000/api/workouts/${id}`, {
+      method: 'DELETE'
+    })
+    workouts.value = workouts.value.filter(vaja => vaja.id != id)
+}catch (error) {
+    console.error("napaka pri brisanju vaje: ", error)
+  }
+}
+
+const urejamoId = ref(null)
+function napolniObrazec(workout) {
+  novaVaja.value = {
+    name: workout.name,
+    reps: workout.reps,
+    calories: workout.calories
+  }
+  urejamoId.value = workout.id
+}
+function preklicUrejanja(){
+  novaVaja.value = {
+    name: '',
+    reps: '',
+    calories: ''
+  }
+  urejamoId.value = null
 }
 
 onMounted(async () => {
